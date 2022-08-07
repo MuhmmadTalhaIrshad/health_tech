5eecde4526b57992a0b912ca7d997d039deefea7
talha <talhaworking5@gmail.com>
2022-08-07 16:10:20 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
+
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
