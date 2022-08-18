86aef0c22f37dc96cb933d1bf6ea7d3a6422c899
talha <talhaworking5@gmail.com>
2022-08-18 23:26:32 +0500

    first commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,41 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 20,
-    pagination: {
-      el: ".swiper-pagination",
-    },
+// ------------------ Navigation  --------------- //
+const nav_icon = document.querySelector('.navigation-mob--icon img');
+const nav      = document.querySelector('.navigation');
+const nav_list = document.querySelector('.navigation-nav');
+
+nav_icon.addEventListener('click', (e)=>{
+    nav.classList.toggle('navigation-open');
 });
 
-var swiper = new Swiper(".mySwiper2", {
-  slidesPerView: 3,
-  spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-  },
+$('.owl-carousel-3').owlCarousel({
+    margin:10,
+    loop:true,
+    autoWidth:true,
+    items:4
+});
+
+var owl = $('.owl-carousel-1');
+owl.owlCarousel({
+    loop:true,
+    nav:false,
+    margin:25,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
 });
