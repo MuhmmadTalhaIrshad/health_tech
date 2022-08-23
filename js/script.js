a14222ae8322bdb05ca4ac19bfb261c24149f2c3
talha <talhaworking5@gmail.com>
2022-08-23 14:56:28 +0500

    3rd commit

--- js/script.js
+++ js/script.js
@@ -7,6 +7,26 @@ nav_icon.addEventListener('click', (e)=>{
     nav.classList.toggle('navigation-open');
 });
 
+// ------------------ Slider 2  ------------------ //
+var owl = $('.owl-carousel-2');
+owl.owlCarousel({
+    loop:true,
+    // nav:true,
+    margin:20,
+    responsive:{
+      0:{
+        items:2
+    },
+    600:{
+        items:4
+    },
+    1000:{
+        items:6
+    }
+    }
+});
+
+// ------------------ Slider 3  ------------------ //
 $('.owl-carousel-3').owlCarousel({
     margin:10,
     loop:true,
@@ -14,6 +34,7 @@ $('.owl-carousel-3').owlCarousel({
     items:4
 });
 
+// ------------------ Slider 1  ------------------ //
 var owl = $('.owl-carousel-1');
 owl.owlCarousel({
     loop:true,
@@ -24,10 +45,10 @@ owl.owlCarousel({
             items:1
         },
         600:{
-            items:4
+            items:3
         },
         1000:{
-            items:5
+            items:4
         }
     }
 });
@@ -39,3 +60,20 @@ owl.on('mousewheel', '.owl-stage', function (e) {
     }
     e.preventDefault();
 });
+
+/// ------------------ Slider dots of testimonial section  ------------------ //
+const dot_1 = document.querySelector('.label-1');
+const dot_2 = document.querySelector('.label-2');
+const dot_3 = document.querySelector('.label-3');
+
+const active_dot = function(dot1, dot2, dot3){
+    dot1.addEventListener('click', () => {
+        dot2.classList.remove('dot-active');
+        dot3.classList.remove('dot-active');
+        dot1.classList.add('dot-active');
+    });
+}
+
+active_dot(dot_1, dot_2, dot_3);
+active_dot(dot_2, dot_1, dot_3);
+active_dot(dot_3, dot_2, dot_1);
